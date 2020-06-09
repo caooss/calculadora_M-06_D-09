44e746dff29b470bebce5349aad9eef9dc813e22
caooss <rafael.r.l.pavan@gmail.com>
2020-06-09 17:03:43 -0300

    Adicionei a operacao de multiplicacao

--- processa.php
+++ processa.php
@@ -4,18 +4,23 @@
 	$valor1 = $_GET['valor1'];
 	$valor2 = $_GET['valor2'];
 	$operacao = $_GET['operacao'];
+	$resultado=0;
 
 	// Processamento
 	switch($operacao) {
-		
+
 		case '+': $resultado = $valor1 + $valor2;
 				  echo "Resultado: $resultado";
 				  break;
-		
+
 		case '-': $resultado = $valor1 - $valor2;
 				  echo "Resultado: $resultado";
 				  break;
-		
+
+		case '*': $resultado=$valor1*$valor2;
+				  echo "Resultado: $resultado";
+				  break;
+
 	}
 
 ?>
