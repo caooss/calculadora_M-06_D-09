a21dc88864d2621736c4a71272a8b4b09d4597b0
Ricardo Nogueira <ricardo@ifsp.edu.br>
2020-06-09 16:29:54 -0300

    Foi acrescentado as operacoes de adicao e subtracao na Calculadora

--- processa.php
+++ processa.php
@@ -1,6 +1,21 @@
 <?php
 
+	// Entrada
+	$valor1 = $_GET['valor1'];
+	$valor2 = $_GET['valor2'];
+	$operacao = $_GET['operacao'];
 
-
+	// Processamento
+	switch($operacao) {
+		
+		case '+': $resultado = $valor1 + $valor2;
+				  echo "Resultado: $resultado";
+				  break;
+		
+		case '-': $resultado = $valor1 - $valor2;
+				  echo "Resultado: $resultado";
+				  break;
+		
+	}
 
 ?>
